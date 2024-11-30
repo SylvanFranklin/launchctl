1a14ca8f06425ce04acc170054d97f5e852dfed8
SylvanFrankin <sylvanfranklin@icloud.com>
2024-11-30 11:42:00 -0500

    testing suite

---
+++ src/tests/service.rs
@@ -0,0 +1,60 @@
+use crate::Service;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creation_basic() {
+        let service = Service::builder()
+            .name("com.<owner name>.<binary name>")
+            .build();
+
+        assert_eq!(service.name, "com.<owner name>.<binary name>");
+        assert_eq!(service.uid, "501");
+        assert_eq!(service.domain_target, "gui/501");
+        assert_eq!(
+            service.service_target,
+            "gui/501/com.<owner name>.<binary name>"
+        );
+        assert_eq!(
+            service.plist_path,
+            "~/Library/LaunchAgents/com.<owner name>.<binary name>.plist"
+        );
+        assert_eq!(
+            service.error_log_path,
+            "/tmp/com.<owner name>.<binary name>_501.err.log"
+        );
+        assert_eq!(
+            service.out_log_path,
+            "/tmp/com.<owner name>.<binary name>_501.out.log"
+        );
+    }
+    #[test]
+    fn creation_advanced() {
+        let service = Service::builder()
+            .name("some weird unconventional name")
+            .uid("401")
+            .build();
+
+        assert_eq!(service.name, "some weird unconventional name");
+        assert_eq!(service.uid, "401");
+        assert_eq!(service.domain_target, "gui/401");
+        assert_eq!(
+            service.service_target,
+            "gui/401/some weird unconventional name"
+        );
+        assert_eq!(
+            service.plist_path,
+            "~/Library/LaunchAgents/some weird unconventional name.plist"
+        );
+        assert_eq!(
+            service.error_log_path,
+            "/tmp/some weird unconventional name_401.err.log"
+        );
+        assert_eq!(
+            service.out_log_path,
+            "/tmp/some weird unconventional name_401.out.log"
+        );
+    }
+}
